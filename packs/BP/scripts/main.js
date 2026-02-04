82c9ee0cc05d64e9b18891d706b8448430e56012
Mizab <mizabmansari@gmail.com>
2026-02-04 22:17:29 +0530

    Implemented the taming logic

--- packs/BP/scripts/main.js
+++ packs/BP/scripts/main.js
@@ -1,40 +1,57 @@
-import { world, system } from "@minecraft/server";
+import { world, EntityTameableComponent } from "@minecraft/server";
 
 const COINS_TO_BE_GIVEN = 5;
 
 world.afterEvents.playerInteractWithEntity.subscribe((event) => {
+  // Variables
   const { player, target, itemStack } = event;
+  const headLocation = target.getHeadLocation();
+  const randomPos = {
+    x: headLocation.x + (Math.random() - 0.5) * 1.2,
+    y: headLocation.y + Math.random() * 0.5 + 0.2,
+    z: headLocation.z + (Math.random() - 0.5) * 1.2,
+  };
 
+  // Check if the target is an iron golem
   if (target.typeId === "minecraft:iron_golem" && itemStack?.typeId === "bdguard:coin") {
     // Count the coins
     const prevCoin = target.getDynamicProperty("coinsGiven") || 0;
     target.setDynamicProperty("coinsGiven", prevCoin + 1);
 
     // Display particles and play sound
-    particlesAndSounds(target);
+    particlesAndSounds(target, randomPos, headLocation);
 
     // Once 5 coins are reached
     if (prevCoin + 1 >= COINS_TO_BE_GIVEN) {
-      console.warn("CONVERTED");
+      transformMob(target, player, randomPos);
     }
-
-    console.warn(target.getDynamicProperty("coinsGiven"));
   }
 });
 
-function particlesAndSounds(target) {
-  const headLocation = target.getHeadLocation();
-
+function particlesAndSounds(target, randomPos, headLocation) {
   for (let i = 0; i < 8; i++) {
-    const randomPos = {
-      x: headLocation.x + (Math.random() - 0.5) * 1.2,
-      y: headLocation.y + Math.random() * 0.5 + 0.2,
-      z: headLocation.z + (Math.random() - 0.5) * 1.2,
-    };
     target.dimension.spawnParticle("minecraft:villager_happy", randomPos);
   }
   target.dimension.playSound("random.orb", headLocation, {
     pitch: 0.5,
     volume: 1.0,
   });
+}
+
+function transformMob(target, player, randomPos) {
+  // transformMob(target, owner, randomPos);
+  const bodyguard = target.dimension.spawnEntity("bdguard:bodyguard", target.location);
+  bodyguard.getComponent(EntityTameableComponent.componentId)?.tame(player);
+
+  // Play sounds and particles
+  target.dimension.playSound("random.level_up", target.location, {
+    pitch: 1.0,
+    volume: 1.0,
+  });
+  for (let i = 0; i < 8; i++) {
+    target.dimension.spawnParticle("minecraft:heart", randomPos);
+  }
+
+  // Remove the entity
+  target.remove();
 }
